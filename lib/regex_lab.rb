1973e52ffd1ecaa1680b4502ba5cdb3df21db725
Jae Yong Kim <kim900180@gmail.com>
2018-07-26 20:44:12 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if "#{text}".scan(/[A-Z]\w*[.]/) != nil
+  if "#{text}".match(/[A-Z]\w*[.]/) != nil
     return true
   elsif "#{text}".match(/[A-Z]\w*[.]/) == nil
     return false
