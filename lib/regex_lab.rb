b8004ce78fd60e1f4052f089f7b853d6b0fff33f
Jae Yong Kim <kim900180@gmail.com>
2018-07-26 20:58:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,18 +1,13 @@
 def starts_with_a_vowel?(word)
-  # if "#{word}".match(/\b[aeiouAEIOU]/) != nil
-  #   return true
-  # elsif "#{word}".match(/\b[^aeiouAEIOU\W]/) != nil
-  #   return false
-  # end
   word.match(/\b[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  return "#{text}".scan(/un\w*ing/)
+  return text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-  return "#{text}".scan(/\b\w{5}\b/)
+  return text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
