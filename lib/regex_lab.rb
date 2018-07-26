907d50c959d2814ddb66f81cd7d91b44e6f4a39f
Jae Yong Kim <kim900180@gmail.com>
2018-07-26 20:57:19 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,11 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if "#{text}".match(/[A-Z]\w*[.]/) != nil
-    return true
-  elsif "#{text}".match(/[a-z]\w*[.]/) != nil
-    return false
-  end
+  text.match(/[A-Z]\w*[.]/) ? true : false
 end
 
 def valid_phone_number?(phone)
